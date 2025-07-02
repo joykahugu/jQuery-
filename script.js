776ee6d0ebf5f0faed8b6cb5fa9b6fe6997f72ec
joykahugu <joynimu1@gmail.com>
2025-07-02 12:44:53 +0300

    learning jQuery

---
+++ script.js
@@ -0,0 +1,9 @@
+ //$("button").on("click", function () {
+           // $("p").toggle();
+       // }); //nby default, toggle will show the hidden p tag ie. the goodbye one 
+
+
+ $( "img" ).on( "click", function() {
+  $( ".woodchair" ).toggle();
+} );
+ 
